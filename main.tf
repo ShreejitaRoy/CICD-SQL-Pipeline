4d8ca7a9f69a33f0952d2eb223acb96f7420f412
Shreejita <shree@abc.com>
2023-02-24 14:40:21 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "google_sql_database" "database" {
+  name     = "tf-sql-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+
+resource "google_sql_database_instance" "instance" {
+  name             = "tf-sql-instance"
+  region           = "asia-south2-a"
+  database_version = "POSTGRES_14"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+}
+
+resource "google_sql_user" "users" {
+  name     = var.name
+  password = var.password
+  instance = google_sql_database_instance.instance.name
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "project" {}
+
+variable "region" {
+    default = "asia-south2-a"
+}
